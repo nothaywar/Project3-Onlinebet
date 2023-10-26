645a54c4365a2b960090233be1dcbc6743a448ba
Haywar Zhu <haywar@Haywars-MacBook-Air.local>
2023-10-26 19:00:53 -0400

    add st function for betting

--- bet.py
+++ bet.py
@@ -2,9 +2,269 @@ import streamlit as st
 from web3 import Web3
 
 # Initialize Web3
-web3 = Web3(Web3.HTTPProvider("Your-Web3-Provider-URL"))  # Replace with your Ethereum provider URL
-contract_address = "Your-Contract-Address"  # Replace with your contract address
-abi = [...]  # Replace with your contract ABI
+web3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))  
+contract_address = "0x8467364C3e1D318cD9a4150BD8Fb660f3E0307D9"  
+abi = """[
+	{
+		"inputs": [],
+		"name": "claimWinnings",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "bool",
+				"name": "teamAWon",
+				"type": "bool"
+			}
+		],
+		"name": "decideOutcome",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "bool",
+				"name": "forTeamA",
+				"type": "bool"
+			}
+		],
+		"name": "placeBet",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "bool",
+				"name": "open",
+				"type": "bool"
+			}
+		],
+		"name": "toggleBetting",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_feeRecipient",
+				"type": "address"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "withdraw",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "admin",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "betsTeamA",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "betsTeamB",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "bettingOpen",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "feeRecipient",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getAdmins",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "contractAdmin",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "feeReceiver",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "bettor",
+				"type": "address"
+			}
+		],
+		"name": "getBetDetails",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "betTeamA",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "betTeamB",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getOutcome",
+		"outputs": [
+			{
+				"internalType": "enum Betting.Outcome",
+				"name": "",
+				"type": "uint8"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getTotalBets",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "totalBetsA",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "totalBetsB",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "outcome",
+		"outputs": [
+			{
+				"internalType": "enum Betting.Outcome",
+				"name": "",
+				"type": "uint8"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "totalBetsTeamA",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "totalBetsTeamB",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]""" 
 
 contract = web3.eth.contract(address=contract_address, abi=abi)
 
@@ -13,20 +273,25 @@ st.title("Betting DApp")
 
 # Define Streamlit functions to interact with the contract
 def place_bet(for_team_a, amount):
-    # Write your code to interact with the placeBet function here
-    pass
+    bet_in_wei = Web3.toWei(amount, 'ether')
+    tx = {
+        "from": web3.eth.defaultAccount,
+        "value": bet_in_wei
+    }
+    contract.functions.placeBet(for_team_a).transact(tx)
+    st.success("Bet placed successfully!")
 
 def decide_outcome(team_a_won):
-    # Write your code to interact with the decideOutcome function here
-    pass
+    tx = {"from": web3.eth.defaultAccount}
+    contract.functions.decideOutcome(team_a_won).transact(tx)
+    st.success("Outcome decided!")
 
 def claim_winnings():
-    # Write your code to interact with the claimWinnings function here
-    pass
+    tx = {"from": web3.eth.defaultAccount}
+    contract.functions.claimWinnings().transact(tx)
+    st.success("Winnings claimed!")
 
-def set_odds(odds_team_a, odds_team_b):
-    # Write your code to interact with the setOdds function here
-    pass
+# Removed set_odds function as it was not in the provided contract
 
 # Calculate potential winnings
 def calculate_winnings(for_team_a, amount):
@@ -55,12 +320,6 @@ if st.sidebar.checkbox("Decide Outcome"):
     if st.sidebar.button("Decide"):
         decide_outcome(team_a_won == "Yes")
 
-if st.sidebar.checkbox("Set Odds"):
-    odds_team_a = st.sidebar.number_input("Odds for Team A")
-    odds_team_b = st.sidebar.number_input("Odds for Team B")
-    if st.sidebar.button("Set Odds"):
-        set_odds(odds_team_a, odds_team_b)
-
 st.sidebar.header("Calculate Potential Winnings")
 if st.sidebar.button("Calculate Potential Winnings"):
     potential_winnings = calculate_winnings(for_team_a == "Team A", amount)
@@ -71,16 +330,11 @@ if st.sidebar.button("Claim Winnings"):
     claim_winnings()
 
 st.sidebar.markdown("Note: You must have MetaMask or a compatible Ethereum wallet to use this DApp.")
-
 st.sidebar.info("Connect your Ethereum wallet to interact with the contract.")
 
 # Display contract data
 st.write("Contract Data")
 st.write(f"Admin Address: {contract.functions.admin().call()}")
-st.write(f"Odds for Team A: {contract.functions.oddsTeamA().call()}")
-st.write(f"Odds for Team B: {contract.functions.oddsTeamB().call()}")
-st.write(f"Outcome: {contract.functions.outcome().call()}")
+st.write(f"Outcome: {contract.functions.getOutcome().call()}")
 st.write(f"Total Bets on Team A: {contract.functions.totalBetsTeamA().call()} ETH")
 st.write(f"Total Bets on Team B: {contract.functions.totalBetsTeamB().call()} ETH")
-
-# Add more information about the contract here as needed
